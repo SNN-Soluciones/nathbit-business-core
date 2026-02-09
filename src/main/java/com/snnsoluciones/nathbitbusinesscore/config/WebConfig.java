e70dd9a0770eb2f1c2d6c355128e431c6a6e5669
Andres Mayorga Espinoza <andres.mayorgaespinoza@nordstrom.com>
2026-02-09 10:18:58 -0600

    refactor: update hibernate ddl-auto setting to none in application.yml

--- src/main/java/com/snnsoluciones/nathbitbusinesscore/config/WebConfig.java
+++ src/main/java/com/snnsoluciones/nathbitbusinesscore/config/WebConfig.java
@@ -19,6 +19,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         // Registrar el TenantInterceptor para todas las rutas
         registry.addInterceptor(tenantInterceptor)
-                .addPathPatterns("/v2/api/business**");  // Solo rutas del negocio
+                .addPathPatterns("/**");  // Solo rutas del negocio
     }
 }
