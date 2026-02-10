40963f268e3d9e93440fb3d74fc551eb8fcca98e
Andres Mayorga Espinoza <andres.mayorgaespinoza@nordstrom.com>
2026-02-10 01:59:47 -0600

    arreglamos productos v2 implementamos info y config para contexto

--- src/main/java/com/snnsoluciones/nathbitbusinesscore/controller/TenantController.java
+++ src/main/java/com/snnsoluciones/nathbitbusinesscore/controller/TenantController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/tenant")
+@RequestMapping("/tenant")
 @RequiredArgsConstructor
 public class TenantController {
 
